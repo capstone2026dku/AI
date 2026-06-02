17426d749b21202654e72b271ef178d12d418a73
YooJunHyuk <a01091040305@gmail.com>
2026-06-02 18:46:02 +0900

    [feat/#7] recommend.py API 코드 추가

--- src/api/recommend.py
+++ src/api/recommend.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 from typing import List
 from ai.preference import get_preference
 from ai.weather import get_weather
+from ai.cooking_time import get_cooking_time, update_cooking_time
 
 router = APIRouter()
 
@@ -15,8 +16,20 @@ class Menu(BaseModel):
 class Request(BaseModel):
     menu: List[str]
 
+class CookingTimeRequest(BaseModel):
+    menu: str
+    orderCount: int
+
+class CookingTimeUpdateRequest(BaseModel):
+    menu: str
+    orderCount: int
+    actualTime: int
+
 class Response(BaseModel):
     menu: List[Menu]
+
+class CookingTimeResponse(BaseModel):
+    cookingTime: int
 
 @router.post("/preference", response_model = Response)
 def preference_res(req: Request):
@@ -29,3 +42,15 @@ def weather_res():
     recommendations = get_weather()
 
     return {"menu": recommendations[:3]}
+
+@router.post("/cooking_time", response_model = CookingTimeResponse)
+def cooking_time_res(req: CookingTimeRequest):
+    res = get_cooking_time(req.menu, req.orderCount)
+
+    return {"cookingTime": res}
+
+@router.post("/cooking_time_update")
+def cooking_time_update_res(req: CookingTimeUpdateRequest):
+    update_cooking_time(req.menu, req.orderCount, req.actualTime)
+
+    return {"message": "Cooking time is updated"}
